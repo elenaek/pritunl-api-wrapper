b1a05f0bd4c81beac8167f6b10c27e7b5e8c2e86
Keane Le <keanele12345@gmail.com>
2020-02-02 15:33:28 -0800

    remove constants

--- constants/actions.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    LIST: "LIST",
-    GET: "GET",
-    CREATE: "CREATE",
-    DELETE: "DELETE",
-    UPDATE: "UPDATE"
-}
--- constants/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    ACTIONS: require("./actions")
-}
